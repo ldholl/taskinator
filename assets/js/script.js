54379b3d99dd56f30d3182f0939669bde3e69ee1
ldholl <lacy.d.holleman@gmail.com>
2022-08-17 15:17:56 -0400

    let users input their own tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,24 @@ var createTaskHandler = function (event){
 
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    //add html content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    tasksToDoEl.appendChild(taskInfoEl);
+
 }
 
 //adding the .addEventListener method to the formEl object. Assigning "subit (a form-specific event that listens for both buttons with a type attribute of 'submit' and keyboard enters) 
