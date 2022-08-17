f2edbdc4d1ada81686e630a98c61dba73699f2bb
ldholl <lacy.d.holleman@gmail.com>
2022-08-17 11:27:41 -0400

    changed from click event to submit event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
+
+//tells the JS to find the form element in the page and save it to the variable formEl so we can interact with it and target its child elements
+var formEl = document.querySelector("#task-form");
 
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (){
+var createTaskHandler = function (event){
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
-
+//adding the .addEventListener method to the formEl object. Assigning "subit (a form-specific event that listens for both buttons with a type attribute of 'submit' and keyboard enters) 
+formEl.addEventListener("submit", createTaskHandler);
